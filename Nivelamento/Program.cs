31f9cc47133557be6c4aca33ac083e7c0fd45230
bmatox <araujobrunomatos@gmail.com>
2024-01-03 15:51:42 -0300

    Update.

---
+++ Nivelamento/Program.cs
@@ -0,0 +1,16 @@
+﻿using Nivelamento;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Triangulo x = new Triangulo(3.00, 4.00, 5.00);
+        Triangulo y = new Triangulo(7.5, 4.5, 4.02);
+
+        x.Area();
+        y.Area();
+
+       x.maiorArea(x, y);
+
+    }
+}
---
+++ Nivelamento/Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace Nivelamento
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+        public double areaX;
+        public double areaY;
+
+        public Triangulo(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+
+        }
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return System.Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        public void maiorArea(Triangulo x, Triangulo y)
+        {
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("Area de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior area: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior area: Y");
+            }
+        }
+    }
+}
