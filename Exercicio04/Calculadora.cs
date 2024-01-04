7e91d37fc6bc7585d926b62bfae6dbf2eb3b1a84
bmatox <araujobrunomatos@gmail.com>
2024-01-04 20:46:55 -0300

    Tópico 01 - Nivelamento.

---
+++ Exercicio04/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿// Nivelamento - à partir do capítulo de classes, atributos, métodos, membros estáticos.
+/*Fazer um programa para ler um valor numérico qualquer, e daí mostrar
+quanto seria o valor de uma circunferência e do volume de uma esfera
+para um raio daquele valor. Informar também o valor de PI com duas
+casas decimais.*/
+
+namespace Exercicio04
+{
+    internal class Calculadora
+    {
+        public static double Pi = 3.14; // o static indica que tanto esse atributo quanto os métodos são membros estáticos. Um membro estático é um membro que pertence à própria classe, em vez de a um objeto específico da classe.
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
--- Exercicio04/Program.cs
+++ Exercicio04/Program.cs
@@ -1,8 +1,17 @@
-﻿// Nivelamento - à partir do capítulo de classes, atributos, métodos, membros estáticos.
+﻿using Exercicio04;
+using System.Globalization;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Entre o valor do raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double circ = Calculadora.Circunferencia(raio); // não é preciso instanciar um objeto, pois o método estático pertence a própria classe e não a um objeto. Sendo assim, é possível chamar o método digitando o próprio nome da classe seguindo pelo nome do método.
+        double volume = Calculadora.Volume(raio);
+        Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2",
+        CultureInfo.InvariantCulture));
     }
 }
