ec25dbf668e09190aff8cc85fee29f12a047105c
bmatox <araujobrunomatos@gmail.com>
2024-01-04 10:35:39 -0300

    Update.

--- Exercicio02/Produto.cs
+++ Exercicio02/Produto.cs
@@ -3,5 +3,35 @@ namespace Exercicio02
 {
     internal class Produto
     {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string Nome, double Preco, int Quantidade)
+        {
+            this.Nome = Nome;
+            this.Preco = Preco;
+            this.Quantidade = Quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int Quantidade)
+        {
+            this.Quantidade += Quantidade;
+        }
+
+        public void RemoverProdutos(int Quantidade)
+        {
+            this.Quantidade -= Quantidade;
+        }
+
+        public void MostraDadosDoProduto()
+        {
+            Console.WriteLine("Dados atualizados do produto: " + Nome + ", R$ " + Preco + ", " + Quantidade + " unidades" + ", " + "Total: " + "R$ " + ValorTotalEmEstoque());
+        }
     }
 }
--- Exercicio02/Program.cs
+++ Exercicio02/Program.cs
@@ -1,7 +1,14 @@
-﻿internal class Program
+﻿using Exercicio02;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Produto produto = new Produto("TV", 900.00, 10);
+        produto.MostraDadosDoProduto();
+        produto.AdicionarProdutos(5);
+        produto.MostraDadosDoProduto();
+        produto.RemoverProdutos(3);
+        produto.MostraDadosDoProduto();
     }
 }
