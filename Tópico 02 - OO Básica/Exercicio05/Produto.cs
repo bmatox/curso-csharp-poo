beddd5d86d830e262f807dadd4e57e0e930698ca
bmatox <araujobrunomatos@gmail.com>
2024-01-05 00:16:53 -0300

    Update.

--- Tópico 02 - OO Básica/Exercicio05/Produto.cs
+++ Tópico 02 - OO Básica/Exercicio05/Produto.cs
@@ -1,25 +1,24 @@
-﻿using System.Globalization;
+﻿// OO Básico - Aula sobre auto properties.
+using System.Globalization;
 
 namespace Exercicio05
 {
     internal class Produto
     {
-        private string _nome;
-        public double Preco { get; private set; }
+        private string _nome; // é uma boa prática manter o atributo privado em minúsculo e, além disso, incializá-lo com o "_".
+        public double Preco { get; private set; } // propertie sendo usada para simplificar o código, privado no set porque ele nao deve alterar o atributo preço na classe principal. 
         public double Quantidade { get; private set; }
-        public Produto()
-        {
-        }
+       
         public Produto(string nome, double preco, int quantidade)
         {
             _nome = nome;
             Preco = preco;
             Quantidade = quantidade;
         }
-        public string Nome
+        public string Nome // propertie comum com get e set determinados no corpo da implementação.
         {
             get { return _nome; }
-            set
+            set // como existe uma regra que determina a alteração do nome, o atributo continua sendo usado como private. 
             {
                 if (value != null && value.Length > 1)
                 {
