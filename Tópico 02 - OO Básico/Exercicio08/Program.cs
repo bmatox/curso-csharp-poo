34e5d549ea6df67e4f3d417ffa1a364d0a932791
bmatox <araujobrunomatos@gmail.com>
2024-01-11 12:19:02 -0300

    Update.

---
+++ Tópico 02 - OO Básico/Exercicio08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+
+        list.Add("Bruno");
+        list.Add("Bob");
+        list.Add("Ana");
+        list.Add("Nélio");
+
+
+        foreach (string nome in list)
+        {
+            Console.WriteLine(nome);
+        }
+    }
+}
