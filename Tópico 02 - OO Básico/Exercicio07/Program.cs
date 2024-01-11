2e4a268a1bbd639b4947205dfd37d5edb26c5905
bmatox <araujobrunomatos@gmail.com>
2024-01-10 23:10:41 -0300

    Update.

---
+++ Tópico 02 - OO Básico/Exercicio07/Program.cs
@@ -0,0 +1,34 @@
+﻿using Exercicio07;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Pensionato[] pensionato = new Pensionato[n];
+        
+        for (int i = 0; i < n; i++)
+        {   
+            int numero = int.Parse(Console.ReadLine());
+            string nome = Console.ReadLine();
+            string email = Console.ReadLine();
+            pensionato[i] = new Pensionato(numero, nome, email);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("\nAluguel #" + (i+1));
+            Console.WriteLine("Nome: " + pensionato[i].NomeDoEstudante);
+            Console.WriteLine("Email: " + pensionato[i].EmailDoEstudante);
+            Console.WriteLine("Quarto: " + pensionato[i].NumerodoQuarto);
+        }
+
+        Console.WriteLine("\nQuartos ocupados:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(pensionato[i].NumerodoQuarto + ": " + pensionato[i].NomeDoEstudante + ", " + pensionato[i].EmailDoEstudante);
+        }        
+
+    }
+}
