1844a4b49472faf91bb124eb215c113503184c48
bmatox <araujobrunomatos@gmail.com>
2024-01-29 15:01:09 -0300

    Update.

---
+++ Tópico 03 - OO Intermediário/Exercicio10/Entidades/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Exercicio10.Entidades.Enums; // precisou adicionar para buscar o atributo do tipo enum, que estava dentro de outro namespace.
+
+namespace Exercicio10.Entidades
+{
+    internal class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public StatusDoPedido Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+
+    }
+}
