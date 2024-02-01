2fd84e1ddcf8c6b801f83c56c87cd24cd7e087d4
bmatox <araujobrunomatos@gmail.com>
2024-02-01 02:07:27 -0300

    Update.

--- Tópico 03 - OO Intermediário/Exercicio11/Program.cs
+++ Tópico 03 - OO Intermediário/Exercicio11/Program.cs
@@ -1,6 +1,7 @@
 ﻿/* Exercício resolvido 01 (Composição e Enum) - Ler os dados de um trabalhador com N contratos (N fornecido pelo usuário). Depois, solicitar
 do usuário um mês e mostrar qual foi o salário do funcionário nesse mês. */
 using Exercicio11.Entidades;
+using Exercicio11.Entidades.Enums;
 using System.Diagnostics.Contracts;
 using System.Diagnostics.Metrics;
 
@@ -10,12 +11,17 @@ internal class Program
     {
         Console.WriteLine("Enter department's name: ");
         string name = Console.ReadLine();
-        Departamento departamento = new Departamento(name);
+        Departamento dep = new Departamento(name);
 
         Console.WriteLine("Enter worker data: ");
         Console.WriteLine("Name: ");
         string name1 = Console.ReadLine();
         Console.WriteLine("Level(Junior / MidLevel / Senior): ");
-     
+        string nivel = Console.ReadLine();
+        NivelDoTrabalhador nt = Enum.Parse<NivelDoTrabalhador>(nivel);
+        Console.WriteLine("Base salary: ");
+        double salariobase = double.Parse(Console.ReadLine());
+        Trabalhador trabalhador = new Trabalhador(name1, nt, salariobase, dep);
+
     }
 }
